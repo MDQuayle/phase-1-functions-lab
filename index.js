3e62096e1c95b8df566ff9f82b92f192e4fcdb50
mdquayle <misternopants@gmail.com>
2021-06-09 17:53:55 -0400

    commit tree e97501174e3aa78cc387d27f7c1e3e89ea8aaa68

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocksNum){
+    return Math.abs(blocksNum-42);
+}
+
+function distanceFromHqInFeet(feetNum){
+    return distanceFromHqInBlocks(feetNum)*264;
+}
+
+function distanceTravelledInFeet(num1,num2){
+    let totalBlocks = Math.abs(num1-num2)
+    return totalBlocks * 264;
+
+}
+
+function calculatesFarePrice(num1,num2){
+    let distance = distanceTravelledInFeet(num1,num2);
+    if (distance < 400) {
+        return 0
+    }
+    else if (distance > 400 && distance <= 2000){
+        return (distance-400) * .02
+    }
+    else if (distance > 2000 && distance < 2500){
+        return 25
+    }
+    else {
+        return "cannot travel that far"
+    }
+}
